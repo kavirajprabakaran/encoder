d54ca07e5c2b59ea7edf45264324df2d5f1355ab
kavirajprabakaran <kaviraj0228@gmail.com>
2025-12-11 21:56:06 +0530

    Add files via upload

---
+++ encodder.v
@@ -0,0 +1,7 @@
+module encodder(a0,a1,a2,d0,d1,d2,d3,d4,d5,d6,d7);
+input d0,d1,d2,d3,d4,d5,d6,d7;
+output a0,a1,a2;
+assign a0 = d1|d3|d5|d7;
+assign a1 =d2|d3|d6|d7;
+assign a2 =d4|d5|d6|d7;
+endmodule
